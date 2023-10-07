0ab1ad4e2ddb7e284d952d42c3d0ddf725834716
Fuyula <fuyulanodahijos@gmail.com>
2023-10-07 19:13:11 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App" data-theme="cupcake">
+        <button className="btn">Hello daisyUI</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
